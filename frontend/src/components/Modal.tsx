75e860e93e3eebcbb3cd021b4c6761d705eb19df
seokjunh <seokjunh97@gmail.com>
2025-09-29 15:52:40 +0900

    update

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -137,6 +137,13 @@ const Modal = ({
         `${API_URL}/get?roomName=${roomName}&date=${formatSelectDate}`,
       );
 
+      if (!response.ok) {
+      // 에러 처리: 상태 코드가 200~299가 아닌 경우
+      const text = await response.text();
+      console.error('Fetch error:', text);
+      return;
+      }
+
       const result: IResevation[] = await response.json();
 
       console.log(result)
