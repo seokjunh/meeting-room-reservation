7c707f895f2480ed02199c2855d2625f16b6114f
seokjunh <seokjunh97@gmail.com>
2025-09-29 14:12:00 +0900

    update

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,14 +6,9 @@ from app.model import Reservation
 
 app = FastAPI()
 
-origins = [
-  "http://127.0.0.1:5173",
-  "http://localhost:5173"
-]
-
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=origins,
+  allow_origins=["*"],
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
